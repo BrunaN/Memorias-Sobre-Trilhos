06fbf559940a46e50d75e267b0d9edc6b8b2b58d
BrunaN <brubsnev@gmail.com>
2018-05-26 23:47:39 -0300

    add post na página da estação

--- project/src/app/station-content/station-content.component.ts
+++ project/src/app/station-content/station-content.component.ts
@@ -1,7 +1,11 @@
+import { PostService } from './../services/post.service';
+import { Comment } from './../models/comment.model';
+import { Usuario } from './../models/usuario.model';
 import { NgModel } from '@angular/forms';
 import { Station } from './../models/station.model';
 import { LoginService } from '../services/login.service';
 import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../models/post.model';
 
 @Component({
   selector: 'app-station-content',
@@ -12,11 +16,20 @@ export class StationContentComponent implements OnInit {
 
   @Input() station: Station;
 
-  descriptionInput: string = "";
+  _id: string;
+  user: Usuario;
+  content: string = "";
+  description: string = "";
+  comments: Comment;
 
-  constructor(protected loginService: LoginService) { }
+  constructor(protected loginService: LoginService, private postService: PostService) { }
 
   ngOnInit() {
+  }
+
+  insert(event){
+    event.preventDefault();
+    this.postService.insertPost(new Post(this._id, this.user, this.station, this.content, this.description, this.comments))
   }
 
 }
