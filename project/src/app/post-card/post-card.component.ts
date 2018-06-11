6ce75b950591531981bf94f51cbfb4ab3638ff68
Bruna Neves <brubsnev@gmail.com>
2018-06-11 03:08:26 -0300

    up

--- project/src/app/post-card/post-card.component.ts
+++ project/src/app/post-card/post-card.component.ts
@@ -91,7 +91,7 @@ export class PostCardComponent implements OnInit {
   like(e){
     e.preventDefault();
     this.post.likes.push(this.user);
-    console.log(this.post.likes);
+    
     this.postService.updatePost(this.post)
                 .subscribe(data => {
                   console.log(data);
